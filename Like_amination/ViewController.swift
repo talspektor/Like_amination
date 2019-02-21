498ab0853650f973c2a45ba1e686c23ae6970a65
user140592 <user140592@eu201.local>
2019-02-21 18:49:32 +0000

    Initial Commit

---
+++ Like_amination/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Like_amination
+//
+//  Created by user140592 on 2/21/19.
+//  Copyright © 2019 talspektor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
